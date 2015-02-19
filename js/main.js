8ad898b7616a799e8ebc2a9f955306cbf2ce1452
williamaulson <waulson@gmu.edu>
2015-02-19 14:18:13 -0500

    hyb

--- js/main.js
+++ js/main.js
@@ -243,7 +243,7 @@ window.onload = function()
     	    bloodDict[8] = 'b9';
     	    
     	    statusText = game.add.text(15, 10, 'Player 2 Kills: ' + player2Killed, titleStyle);
-    	    statusText2 = game.add.text(875, 10, 'Player 1 Kills: ' + player1Killed, titleStyle);
+    	    statusText2 = game.add.text(872, 10, 'Player 1 Kills: ' + player1Killed, titleStyle);
     	    
     	    
     	    
